094f2fff48da5972405391c0b147d60df3a4f424
aquiab <aquiabuin@gmail.com>
2023-05-17 20:27:02 -0300

    s

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ $("#mainpage").append(`
     <div id="updub" style="color: green;"> ${updubs} </div>
     <div id="downdub" style="color: red;"> ${downdubs} </div>
   </div>
-`);
+`)
+var ws = new WebSocket('wss://zip.cytu.be:8443/socket.io/?EIO=4&transport=websocket&sid=z-YAa_ZStSxSUgxTOcxS');
+ws.onopen = function() {
+  console.log('WebSocket connection established');
+};
 $('#updub').click(function() {
   updubs += 1
   $('#updub').html(updubs)
+  var message = "dou";
+  ws.send(message);
 })
 $('#downdub').click(function() {
   downdubs += 1
