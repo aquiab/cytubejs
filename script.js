d326624c7c4d2d99df6eb8e087ec853e71b57c35
aqui <42970445+aquiab@users.noreply.github.com>
2023-05-16 23:30:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 console.log('hola')
-const mainpage = document.getElementById("mainpage")
-const dubsDiv = document.createElement('div')
-const upDubsButton = document.createElement('div')
-const downDubsButton = document.createElement('div')
-upDubsButton.innerHTML += '1'
-downDubsButton.innerHTML += '1'
-
-upDubsButton.style.backgroundColor = 'green'
-downDubsButton.style.backgroundColor = 'red'
-
-dubsDiv.appendChild(upDubsButton)
-dubsDiv.appendChild(downDubsButton)
-mainpage.appendChild(dubsDiv)
-$("#mainpage").after('<div><div id="updub" style="color: red;">1</div></div>');
+$("#mainpage").append(`
+  <div class="dubs-wrapper">
+    <div id="updub" style="color: green;"> 1 </div>
+    <div id="downdub" style="color: red;"> 1 </div>
+  </div>
+`);
