e99b4a0d3ec7f9ec67d6bac36e8a9b2e79fe18e2
aquiab <aquiabuin@gmail.com>
2023-05-17 20:03:44 -0300

    e

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 console.log('hola')
+let updubs = 0
+let downdubs = 0
 $("#mainpage").append(`
   <div class="dubs-wrapper">
-    <div id="updub" style="color: green;"> 1 </div>
-    <div id="downdub" style="color: red;"> 1 </div>
+    <div id="updub" style="color: green;"> ${updubs} </div>
+    <div id="downdub" style="color: red;"> ${downdubs} </div>
   </div>
 `);
+$('#updub').attr('onclick', () => updubs += 1);
+$('#downdub').attr('onclick', () => downdubs += 1);
