83a71eba115b8fda522751f1ea53917400ad8227
aqui <42970445+aquiab@users.noreply.github.com>
2023-05-16 23:01:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log('hola')
+const mainpage = document.getElementById("mainpage")
+let imageElement = document.createElement('img');
+imageElement.setAttribute('src','https://www.cnet.com/a/img/resize/8aa032987077903f8ee110feb7323ea4df61d0dd/hub/2022/10/13/383cce3b-5757-436a-8bdb-2665aaafd56a/imessage-text-messages.jpg?auto=webp&fit=crop&height=675&width=1200');
+mainpage.appendChild(imageElement)
