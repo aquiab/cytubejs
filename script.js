b5c273103353aeb4518544f887346105fa3a972c
aquiab <aquiabuin@gmail.com>
2023-05-18 01:28:54 -0300

    ee

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const DOWNDUB_COMMAND = "DOWNDUB"
 
 $(document).ready(function() {
 	$(`span:contains(${UPDUB_COMMAND}), span:contains(${DOWNDUB_COMMAND})`).parent().hide()
-	currentUser = $("#welcome").text().split(',')[1]
+	currentUser = $("#welcome").text().split(',')[1].trim()
 });
 
 $("#leftcontrols").append(`
