f2f90897f4011ec9b3d35891870d074da603085f
aquiab <aquiabuin@gmail.com>
2023-05-18 21:11:22 -0300

    ee

--- script.js
+++ script.js
@@ -23,6 +23,25 @@ $("#leftcontrols").append(`
   </div>
 `)
 
+const targetNode = $("#messagebuffer")
+const config = { attributes: true, childList: true, subtree: true };
+
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === "childList") {
+      console.log("A child node has been added or removed.");
+    } else if (mutation.type === "attributes") {
+      console.log(`The ${mutation.attributeName} attribute was modified.`);
+    }
+  }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
 $("#messagebuffer").on('DOMSubtreeModified', function() {
 	var lastMessageDiv = $("#messagebuffer").children().last()
 	var lastMessageUser = lastMessageDiv.attr("class").split('-')[2]
